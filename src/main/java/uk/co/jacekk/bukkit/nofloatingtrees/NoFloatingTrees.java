880c06f5f712594a9a91e93cd719e19fd9bb3310
Jacek <jacek@kuzemczak.co.uk>
2013-02-11 17:04:15 +0000

    Moved getTree() method to the main plugin class

--- src/main/java/uk/co/jacekk/bukkit/nofloatingtrees/NoFloatingTrees.java
+++ src/main/java/uk/co/jacekk/bukkit/nofloatingtrees/NoFloatingTrees.java
@@ -2,15 +2,18 @@ package uk.co.jacekk.bukkit.nofloatingtrees;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Stack;
 
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 
 import uk.co.jacekk.bukkit.baseplugin.v9_1.BasePlugin;
 import uk.co.jacekk.bukkit.baseplugin.v9_1.config.PluginConfig;
 import uk.co.jacekk.bukkit.nofloatingtrees.commands.NftExecutor;
 import uk.co.jacekk.bukkit.nofloatingtrees.listeners.TreeBreakListener;
 import uk.co.jacekk.bukkit.nofloatingtrees.storage.DecayQueue;
-
 import de.diddiz.LogBlock.Consumer;
 import de.diddiz.LogBlock.LogBlock;
 
@@ -19,7 +22,6 @@ public class NoFloatingTrees extends BasePlugin {
 	public Consumer logblock;
 	
 	public DecayQueue decayQueue;
-	private TreeBreakListener listener;
 	
 	public void onEnable(){
 		super.onEnable(true);
@@ -36,14 +38,37 @@ public class NoFloatingTrees extends BasePlugin {
 		
 		this.scheduler.scheduleSyncRepeatingTask(this, new LogDecayTask(this), 10, 20 * this.config.getInt(Config.DECAY_FREQUENCY));
 		
-		this.listener = new TreeBreakListener(this);
-		this.pluginManager.registerEvents(this.listener, this);
+		this.pluginManager.registerEvents(new TreeBreakListener(this), this);
 		this.permissionManager.registerPermissions(Permission.class);
 		this.commandManager.registerCommandExecutor(new NftExecutor(this));
 	}
 	
 	public void onDisable(){
 		this.decayQueue.save();
+	}
+	
+	private Block[] getSurroundingBlocks(Block block){
+		Block[] blocks = new Block[17];
+		
+		blocks[0] = block.getRelative(BlockFace.UP);
+		blocks[1] = block.getRelative(BlockFace.NORTH);
+		blocks[2] = block.getRelative(BlockFace.NORTH_EAST);
+		blocks[3] = block.getRelative(BlockFace.NORTH_WEST);
+		blocks[4] = block.getRelative(BlockFace.SOUTH);
+		blocks[5] = block.getRelative(BlockFace.SOUTH_EAST);
+		blocks[6] = block.getRelative(BlockFace.SOUTH_WEST);
+		blocks[7] = block.getRelative(BlockFace.EAST);
+		blocks[8] = block.getRelative(BlockFace.WEST);
+		blocks[9] = block.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH);
+		blocks[10] = block.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH_EAST);
+		blocks[11] = block.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH_WEST);
+		blocks[12] = block.getRelative(BlockFace.UP).getRelative(BlockFace.SOUTH);
+		blocks[13] = block.getRelative(BlockFace.UP).getRelative(BlockFace.SOUTH_EAST);
+		blocks[14] = block.getRelative(BlockFace.UP).getRelative(BlockFace.SOUTH_WEST);
+		blocks[15] = block.getRelative(BlockFace.UP).getRelative(BlockFace.EAST);
+		blocks[16] = block.getRelative(BlockFace.UP).getRelative(BlockFace.WEST);
+		
+		return blocks;
 	}
 	
 	/**
@@ -54,7 +79,61 @@ public class NoFloatingTrees extends BasePlugin {
 	 * @return {@link ArrayList<Block>} of tree log blocks, or null
 	 */
 	public ArrayList<Block> getTree(Block block, boolean force){
-		return this.listener.getTree(block, force);
+		World world = block.getWorld();
+		
+		// Not part of the trunk ?
+		Material aboveType = block.getRelative(BlockFace.UP).getType();
+		Material belowType = block.getRelative(BlockFace.DOWN).getType();
+		
+		if (!force && ((belowType != Material.DIRT && belowType != Material.GRASS && belowType != Material.LOG) || aboveType != Material.LOG)){
+			return null;
+		}
+		
+		// Left a floating tree and not part of a house ?
+		Material northType = block.getRelative(BlockFace.NORTH).getType();
+		Material southType = block.getRelative(BlockFace.SOUTH).getType();
+		Material eastType = block.getRelative(BlockFace.EAST).getType();
+		Material westType = block.getRelative(BlockFace.WEST).getType();
+		
+		if (northType == Material.LOG || southType == Material.LOG || eastType == Material.LOG || westType == Material.LOG){
+			return null;
+		}
+		
+		int x = block.getX();
+		int y;
+		int z = block.getZ();
+		
+		for (y = block.getY(); world.getBlockTypeIdAt(x, y, z) != 0; ++y);
+		
+		if (!force){
+			Integer topId = world.getBlockTypeIdAt(x, y - 1, z);
+			
+			if (topId == Material.SNOW.getId()){
+				topId = world.getBlockTypeIdAt(x, y - 2, z);
+			}
+			
+			// No leaf block a the top ?
+			if (topId != Material.LEAVES.getId()){
+				return null;
+			}
+		}
+		
+		ArrayList<Block> logs = new ArrayList<Block>();
+		Stack<Block> queue = new Stack<Block>();
+		
+		logs.add(block);
+		queue.push(block);
+		
+		while (queue.isEmpty() == false){
+			for (Block surrounding : this.getSurroundingBlocks(queue.pop())){
+				if (surrounding.getType() == Material.LOG && logs.contains(surrounding) == false){
+					logs.add(surrounding);
+					queue.push(surrounding);
+				}
+			}
+		}
+		
+		return logs;
 	}
 	
 }
--- src/main/java/uk/co/jacekk/bukkit/nofloatingtrees/listeners/TreeBreakListener.java
+++ src/main/java/uk/co/jacekk/bukkit/nofloatingtrees/listeners/TreeBreakListener.java
@@ -1,12 +1,9 @@
 package uk.co.jacekk.bukkit.nofloatingtrees.listeners;
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -22,96 +19,14 @@ public class TreeBreakListener extends BaseListener<NoFloatingTrees> {
 		super(plugin);
 	}
 	
-	private Block[] getSurroundingBlocks(Block block){
-		Block[] blocks = new Block[17];
-		
-		blocks[0] = block.getRelative(BlockFace.UP);
-		blocks[1] = block.getRelative(BlockFace.NORTH);
-		blocks[2] = block.getRelative(BlockFace.NORTH_EAST);
-		blocks[3] = block.getRelative(BlockFace.NORTH_WEST);
-		blocks[4] = block.getRelative(BlockFace.SOUTH);
-		blocks[5] = block.getRelative(BlockFace.SOUTH_EAST);
-		blocks[6] = block.getRelative(BlockFace.SOUTH_WEST);
-		blocks[7] = block.getRelative(BlockFace.EAST);
-		blocks[8] = block.getRelative(BlockFace.WEST);
-		blocks[9] = block.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH);
-		blocks[10] = block.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH_EAST);
-		blocks[11] = block.getRelative(BlockFace.UP).getRelative(BlockFace.NORTH_WEST);
-		blocks[12] = block.getRelative(BlockFace.UP).getRelative(BlockFace.SOUTH);
-		blocks[13] = block.getRelative(BlockFace.UP).getRelative(BlockFace.SOUTH_EAST);
-		blocks[14] = block.getRelative(BlockFace.UP).getRelative(BlockFace.SOUTH_WEST);
-		blocks[15] = block.getRelative(BlockFace.UP).getRelative(BlockFace.EAST);
-		blocks[16] = block.getRelative(BlockFace.UP).getRelative(BlockFace.WEST);
-		
-		return blocks;
-	}
-	
 	public void processTreeBlockBreak(Block block){
-		ArrayList<Block> tree = getTree(block, false);
+		ArrayList<Block> tree = plugin.getTree(block, false);
 		
 		if (tree != null){
 			for (Block log : tree){
 				plugin.decayQueue.addBlock(log);
 			}
 		}
-	}
-	
-	public ArrayList<Block> getTree(Block block, boolean force){
-		World world = block.getWorld();
-		
-		// Not part of the trunk ?
-		Material aboveType = block.getRelative(BlockFace.UP).getType();
-		Material belowType = block.getRelative(BlockFace.DOWN).getType();
-		
-		if (!force && ((belowType != Material.DIRT && belowType != Material.GRASS && belowType != Material.LOG) || aboveType != Material.LOG)){
-			return null;
-		}
-		
-		// Left a floating tree and not part of a house ?
-		Material northType = block.getRelative(BlockFace.NORTH).getType();
-		Material southType = block.getRelative(BlockFace.SOUTH).getType();
-		Material eastType = block.getRelative(BlockFace.EAST).getType();
-		Material westType = block.getRelative(BlockFace.WEST).getType();
-		
-		if (northType == Material.LOG || southType == Material.LOG || eastType == Material.LOG || westType == Material.LOG){
-			return null;
-		}
-		
-		int x = block.getX();
-		int y;
-		int z = block.getZ();
-		
-		for (y = block.getY(); world.getBlockTypeIdAt(x, y, z) != 0; ++y);
-		
-		if (!force){
-			Integer topId = world.getBlockTypeIdAt(x, y - 1, z);
-			
-			if (topId == Material.SNOW.getId()){
-				topId = world.getBlockTypeIdAt(x, y - 2, z);
-			}
-			
-			// No leaf block a the top ?
-			if (topId != Material.LEAVES.getId()){
-				return null;
-			}
-		}
-		
-		ArrayList<Block> logs = new ArrayList<Block>();
-		Stack<Block> queue = new Stack<Block>();
-		
-		logs.add(block);
-		queue.push(block);
-		
-		while (queue.isEmpty() == false){
-			for (Block surrounding : this.getSurroundingBlocks(queue.pop())){
-				if (surrounding.getType() == Material.LOG && logs.contains(surrounding) == false){
-					logs.add(surrounding);
-					queue.push(surrounding);
-				}
-			}
-		}
-		
-		return logs;
 	}
 	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
