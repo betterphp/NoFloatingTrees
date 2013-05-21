0bebf9083f4220927112abc968c374c0e2075461
Timo Reunanen <parker@friikz.net>
2013-05-21 09:10:09 +0300

    Reformatted code.

--- src/main/java/uk/co/jacekk/bukkit/nofloatingtrees/LogDecayTask.java
+++ src/main/java/uk/co/jacekk/bukkit/nofloatingtrees/LogDecayTask.java
@@ -29,9 +29,9 @@ public class LogDecayTask extends BaseTask<NoFloatingTrees> {
 			if (type != Material.LOG){
 				remove.add(blockLocation);
 			}else if (this.rand.nextInt(100) < plugin.config.getInt(Config.DECAY_CHANCE)){
-                if (plugin.logblock != null){
-            		plugin.logblock.queueBlockBreak("NoFloatingTrees", block.getState());
-            	}
+				if (plugin.logblock != null){
+					plugin.logblock.queueBlockBreak("NoFloatingTrees", block.getState());
+				}
 
 				if (rand.nextInt(100) < plugin.config.getInt(Config.DECAY_DROP_CHANCE)){
 					block.breakNaturally();
